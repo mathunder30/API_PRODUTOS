a1ff864631e71a69d4e1db54a3186d61bbabb86c
mathunder30 <mateustlopes11@gmail.com>
2025-05-12 23:30:36 -0300

    criando classe produtos

---
+++ backend/src/models/Produtos.ts
@@ -0,0 +1,21 @@
+import { Schema, model, Document } from "mongoose";
+
+interface IProduto extends Document {
+    nome: string;
+    categoria: string;
+    preco: number;
+    quantidade: number;
+}
+
+const ProdutoSchema = new Schema<IProduto>({
+    nome:{type: String, required: true},
+    categoria: { type: String, required: true},
+    preco: { type: Number, required: true},
+    quantidade: { type: Number, required: true}
+}, {
+    timestamps: true
+});
+
+const ProdutoModel = model<IProduto>('Produtos', ProdutoSchema);
+
+export default ProdutoModel;
