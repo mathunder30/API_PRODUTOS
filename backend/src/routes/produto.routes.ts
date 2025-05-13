7010de2b331d02bbed8b7b7c39c80f80e6ab93e6
mathunder30 <mateustlopes11@gmail.com>
2025-05-13 20:49:30 -0300

    rotar para criar produto!

--- backend/src/routes/produto.routes.ts
+++ backend/src/routes/produto.routes.ts
@@ -1,2 +1,6 @@
 import  Express  from "express";
+import { criarProduto} from "../controllers/ProdutosControllers";
 const router = Express.Router();
+
+//Rota para criar produto
+router.get('/get_produto', criarProduto);
