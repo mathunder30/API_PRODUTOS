15df395219d01b6c7bc9ad4aa67e893b24e55cdd
mathunder30 <mateustlopes11@gmail.com>
2025-05-12 23:10:02 -0300

    primeiro commit

---
+++ backend/src/server.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import {connectDB} from './config/database';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+app.use('...');
+
+connectDB();
+
+
+const port = process.env.port || 3000;
+app.listen(port, () => console.log(`Servidor rodando na porta ${port}`))
